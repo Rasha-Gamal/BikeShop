97c8b045365a8b10f763b377515344c7ad8a3beb
Rasha-Gamal <44783930+Rasha-Gamal@users.noreply.github.com>
2018-12-11 02:54:09 +0200

    Add files via upload

--- BikeShop/Models/Customer.cs
+++ BikeShop/Models/Customer.cs
@@ -11,25 +11,39 @@ namespace BikeShop.Models
 
         public int CustomerId { get; set; }
 
-        [Required(ErrorMessage = "Name is required")]
-        [Display(Name = "Customer name")]
+        [Required(ErrorMessage ="You must enter your name")]
+        [Display(Name ="Customer Name")]
         public string CustomerName { get; set; }
 
-        [Required(ErrorMessage = "Password is required")]
-        [Display(Name = "Password")]
+        [Required(ErrorMessage = "You must enter a password")]
+        [StringLength(18,ErrorMessage ="Must be 18 characters")]
         public string Password { get; set; }
 
+        [Required(ErrorMessage ="you must enter your E-mail")]
         public string Email { get; set; }
-    
-        public int VisaNumber { get; set; }
+
+        [Required(ErrorMessage = "You must enter a your credit card number")]
+        [Display(Name ="Credit Card Number")]
+        public int CreditCardNumber { get; set; }
+
+        [Required(ErrorMessage = "You must enter a your credit card Expiration date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-mm}",ApplyFormatInEditMode =true)]
+        public DateTime ExpirationDate { get; set; }
+
+        [Required(ErrorMessage = "You must enter cvv")]
+        public int Cvv { get; set; }
 
         public string Phone { get; set; }
 
+        [Required(ErrorMessage = "You must enter a your address")]
         public string Address { get; set; }
 
         public ICollection <Order> OrdersList { get; set; }
-        public ICollection <Product_Customer> CustomerList { get; set; }
+        public ICollection <Product_Customer> ProductList { get; set; }
         
 
     }
+
+    
 }
